1b309bd2f500f3122e4009df49e8856d4b716184
Avi Cavale <avi@shippable.com>
2015-08-16 08:24:10 -0700

    fixing gcloud path

--- layer2.sh
+++ layer2.sh
@@ -3,4 +3,4 @@
 cd /opt/gcloud/google-cloud-sdk && ./install.sh --usage-reporting=false --bash-completion=true --path-update=true
 export PATH=$PATH:/opt/gcloud/google-cloud-sdk/bin
 cd /
-gcloud components update preview
+/opt/gcloud/google-cloud-sdk/bin/gcloud components update preview
